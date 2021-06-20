ed512caa201271a05a0ee9ff4c2cdd3d8cc4d40b
AadiJo <mail8aadi@gmail.com>
2021-06-20 10:14:33 -0500

    added starter file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+
+run = True
+
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
